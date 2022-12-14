ee7af2ea443ec410f17672d61d506097bac663f7
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2022-12-14 17:25:49 +0000

    reputing everything to start

--- models/staging/ga4/base/base_ga4__events.sql
+++ models/staging/ga4/base/base_ga4__events.sql
@@ -134,7 +134,7 @@ renamed as (
         {{ ga4.unnest_key('event_params', 'source') }},
         {{ ga4.unnest_key('event_params', 'medium') }},
         {{ ga4.unnest_key('event_params', 'campaign') }},
-        {{ ga4.unnest_key('event_params', 'content_type') }},
+        {{ ga4.unnest_key('event_params', 'content') }},
         {{ ga4.unnest_key('event_params', 'term') }},
         CASE 
             WHEN event_name = 'page_view' THEN 1
