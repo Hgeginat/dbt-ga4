fd717bcc42b2a08536b5052943edc6a7dc16456e
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2022-12-27 13:47:11 +0000

    overview

--- models/marts/core/dim_ga4_overview.sql
+++ models/marts/core/dim_ga4_overview.sql
@@ -1,11 +1,11 @@
 with group1 as (
     select 
         event_date_dt as date,
-        user_key,
-        count(distinct concat(session_key,ga_session_id)) as num_sessions
+        session_key as session_key,
+        user_key as user_key
 
-    from {{ref('stg_ga4__event_session_start')}}
-    group by 1,2
+    from {{ref('stg_ga4__events')}}
+    group by 1,2,3
 ),
 
 include_user_properties_market as (
@@ -17,9 +17,22 @@ include_user_properties_market as (
     -- If user properties have been assigned as variables, join them on the user_key
     left join {{ref('stg_ga4__user_properties')}} as user_properties using (user_key)
     {% endif %}
+),
+include_user_pseudo_ids as (
+    select 
+    user_pseudo.user_pseudo_id,
+    include_user_properties_market.*,
+    from include_user_properties_market
+    left join {{ref('dim_ga4__sessions')}} as user_pseudo using (user_key)
 )
      
-select * from include_user_properties_market
+select 
+date,
+polestar_market,
+user_pseudo_id,
+user_key,
+session_key
+ from include_user_pseudo_ids
 
 
  
