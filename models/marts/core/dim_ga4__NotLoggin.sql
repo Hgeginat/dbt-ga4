8ed7b3aed11d9ac6e025b7f554a19ffc2eb9930e
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2023-01-05 17:46:59 +0000

    rename

--- models/marts/core/dim_ga4__NotLoggin.sql
+++ models/marts/core/dim_ga4__NotLoggin.sql
@@ -47,7 +47,7 @@ Journey as (
         polestar_market,
         logged_in,
         is_paired,
-      (case when engagement_time_msec > 0 or session_engaged = 1 then user_key else null end) as active_user_key
+       (case when engagement_time_msec > 0 or session_engaged = 1 then user_key else null end) as active_user_key
     
     from include_derived_session_properties2
     where logged_in in ('false','null')
