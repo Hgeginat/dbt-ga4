6b965689284708983bcc9fad4239c40e297c8113
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2023-01-06 18:32:17 +0000

    MM2 finall

--- models/marts/core/dim_ga4__MicroMoments2.sql
+++ models/marts/core/dim_ga4__MicroMoments2.sql
@@ -1,13 +1,14 @@
+
 with screen_name_events as (
     select 
         event_date_dt as date,
         user_key,
+        ga_session_id,
         stream_id, 
         user_pseudo_id,
+        session_key,
         traffic_source_medium,
-         (select value.string_value from unnest(event_params) where key = 'name') as name,
-        (select value.int_value from unnest(event_params) where key = 'ga_session_id') as ga_session_id,
-        (select value.int_value from unnest(event_params) where key = 'ga_session_number') as ga_session_number,
+        (select value.string_value from unnest(event_params) where key = 'name') as name
         (select value.string_value from unnest(user_properties) where key = 'polestar_market') as polestar_market,
         (select value.string_value from unnest(user_properties) where key = 'logged_in') as logged_in,
         (select value.string_value from unnest(user_properties) where key = 'is_paired') as is_paired
@@ -15,26 +16,17 @@ with screen_name_events as (
     where event_name = 'screen_name'
 ),
 
-session_key_added as (
-    select 
-        screen_name_events.*,
-        to_base64(md5(CONCAT(stream_id, user_pseudo_id, CAST(ga_session_id as STRING)))) as session_key,
-        
-        from screen_name_events
-),
-
 include_derived_session_properties as (
     select 
-        session_key_added.*,
+        screen_name_events.*,
         session_properties.engagement_time_msec,
-        session_properties.session_engaged 
-    from session_key_added
+        session_properties.session_engaged
+    from screen_name_events
     {% if var('derived_session_properties', false) %}
     -- If derived user properties have been assigned as variables, join them on the user_key
     left join {{ref('stg_ga4__derived_session_properties')}} as session_properties using (session_key)
     {% endif %}
 ),
-
 micro_moments as (
     select date,
         user_key,
@@ -47,9 +39,7 @@ micro_moments as (
         logged_in,
         is_paired,
         traffic_source_medium,
-        (case when engagement_time_msec > 0 or session_engaged = 1 then user_key else null end) as active_user_key, 
-        engagement_time_msec,
-        session_engaged,
+      (case when engagement_time_msec > 0 or session_engaged = 1 then user_key else null end) as active_user_key,  
       CASE WHEN name like 'App:Post:%' THEN SUBSTR(REGEXP_REPLACE(name, 'App:(Post|post):[0-9]*:', ''),1,30)
            WHEN name like 'App:post:%' THEN SUBSTR(REGEXP_REPLACE(name, 'App:(Post|post):[0-9]*:', ''),1,30)
       END as micro_moment_name
@@ -58,3 +48,20 @@ micro_moments as (
 )
 
 select * from micro_moments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
