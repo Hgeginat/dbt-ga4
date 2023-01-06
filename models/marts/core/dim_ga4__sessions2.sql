193120bb6138d32c06c0ee7897910241844d1d7e
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2023-01-06 14:58:52 +0000

    session 2 added

---
+++ models/marts/core/dim_ga4__sessions2.sql
@@ -0,0 +1,24 @@
+with session_base as (
+    select 
+        event_name,
+        event_date_dt,
+        user_pseudo_id,
+        stream_id,
+        (select value.int_value from unnest(event_params) where key = 'ga_session_id') as ga_session_id,
+        (select value.string_value from unnest(user_properties) where key = 'polestar_market') as polestar_market,
+        (select value.string_value from unnest(user_properties) where key = 'logged_in') as logged_in,
+        (select value.string_value from unnest(user_properties) where key = 'polestar_market') as polestar_market,
+        (select value.string_value from unnest(event_params) where key = 'is_paired') as is_paired,
+
+
+    from {{ref('stg_ga4__events')}}),
+
+with session_base_key_added as (
+    select 
+    session_base.*,
+    to_base64(md5(CONCAT(stream_id, user_pseudo_id, CAST(ga_session_id as STRING)))) as session_key
+
+)
+
+select * from session_base_key_added
+    
