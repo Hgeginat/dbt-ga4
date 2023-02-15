1e78659e5f8643316b683c5cf59942c47518b703
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2023-02-15 19:08:44 +0000

    user engagement time

--- models/marts/core/dim_ga4__SessionTime.sql
+++ models/marts/core/dim_ga4__SessionTime.sql
@@ -10,12 +10,12 @@ with start as (
         (select value.string_value from unnest(user_properties) where key = 'polestar_market') as polestar_market,
         (select value.string_value from unnest(user_properties) where key = 'logged_in') as logged_in,
         (select value.string_value from unnest(user_properties) where key = 'is_paired') as is_paired,
-        max((select value.int_value from unnest(event_params) where key = 'session_engaged')) as session_engaged,
-        max((select value.int_value from unnest(event_params) where key = 'engagement_time_msec'))/1000 as engagement_time_seconds,
+        ((select value.int_value from unnest(event_params) where key = 'engaged_session_event')) as session_engaged,
+        ((select value.int_value from unnest(event_params) where key = 'engagement_time_msec'))/1000 as engagement_time_seconds,
 
     from {{ref('stg_ga4__events')}}
-    group by  
-    1,2,3,4,5,6,7,8,9
+    where event_name="user_engagement"
+   
     
 )
 
