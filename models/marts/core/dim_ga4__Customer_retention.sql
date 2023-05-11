6b7721a0c2e3be58841421f7f4f1fe02c6716cf9
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2023-05-11 14:39:52 +0000

    customer retention

---
+++ models/marts/core/dim_ga4__Customer_retention.sql
@@ -0,0 +1,43 @@
+with first_part_data as (
+
+select 
+event_date_dt,
+active_user_key,
+polestar_market
+
+from {{ref('dim_ga4__Funnel')}}),
+
+second_part_data as (
+
+select 
+active_user_key,
+polestar_market,
+min(event_date_dt) as first_time_seen,
+max(event_date_dt) as Last_time_seen
+
+from first_part_data
+group by 1,2),
+
+last_part_data as (
+    select
+    active_user_key,
+    polestar_market,
+    first_time_seen,
+    Last_time_seen,
+    (CASE  WHEN extract(ISOWEEK from first_time_seen)<10 THEN   extract(ISOYEAR from first_time_seen) || "0" || extract(ISOWEEK from first_time_seen) 
+           ELSE extract(ISOYEAR from first_time_seen) || "" || extract(ISOWEEK from first_time_seen) END) as first_week_seen,
+    (CASE  WHEN extract(ISOWEEK from Last_time_seen) <10 THEN   extract(ISOYEAR from Last_time_seen) || "0" || extract(ISOWEEK from Last_time_seen)
+           ELSE extract(ISOYEAR from Last_time_seen) || "" || extract(ISOWEEK from Last_time_seen) END) as last_week_seen
+    
+
+
+    from second_part_data
+    )
+
+select * from last_part_data 
+
+
+
+
+ 
+
