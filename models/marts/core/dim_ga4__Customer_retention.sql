b806e782fe28b99c1143189a14c2d525d8b38536
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-01-23 20:02:57 +0000

    Getting month inital & final as text

--- models/marts/core/dim_ga4__Customer_retention.sql
+++ models/marts/core/dim_ga4__Customer_retention.sql
@@ -32,6 +32,8 @@ last_part_data as (
            ELSE extract(ISOYEAR from first_time_seen) || "" || extract(ISOWEEK from first_time_seen) END) as first_week_seen,
     (CASE  WHEN extract(ISOWEEK from Last_time_seen) <10 THEN   extract(ISOYEAR from Last_time_seen) || "0" || extract(ISOWEEK from Last_time_seen)
            ELSE extract(ISOYEAR from Last_time_seen) || "" || extract(ISOWEEK from Last_time_seen) END) as last_week_seen,
+    DATE_TRUNC(first_time_seen, MONTH) AS Aquisition_Month,
+    DATE_TRUNC(Last_time_seen, MONTH) AS Latest_Month,
     first_time_seen as inital_date,
     Last_time_seen as date,
     date_diff(CURRENT_DATE, Last_time_seen, DAY) as diff_days
@@ -44,7 +46,18 @@ last_part_data as (
     from second_part_data
     )
 
-select * from last_part_data 
+select *,
+    CASE
+        WHEN EXTRACT(MONTH FROM Aquisition_Month)<10 THEN ( EXTRACT(ISOYEAR FROM Aquisition_Month) || '0' || EXTRACT(MONTH FROM Aquisition_Month))
+        ELSE (EXTRACT(ISOYEAR FROM Aquisition_Month) || '' || EXTRACT(MONTH FROM Aquisition_Month)) 
+    END  AS Aquisition_Month,
+
+    CASE
+        WHEN EXTRACT(MONTH FROM Latest_Month)<10 THEN ( EXTRACT(ISOYEAR FROM Latest_Month) || '0' || EXTRACT(MONTH FROM Latest_Month))
+        ELSE (EXTRACT(ISOYEAR FROM Latest_Month) || '' || EXTRACT(MONTH FROM Latest_Month)) 
+    END  AS Latest_Month
+
+ from last_part_data 
 
 
 
