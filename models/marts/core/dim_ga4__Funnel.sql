183e2594f8611b17c91d10ac25285a8e21018ada
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-01-29 17:32:37 +0000

    checking car owner

--- models/marts/core/dim_ga4__Funnel.sql
+++ models/marts/core/dim_ga4__Funnel.sql
@@ -43,10 +43,10 @@ final_table as (
         polestar_market,
         logged_in,
         is_paired,
-        is_car_owner,
         device_operating_system,
         app_info_version,
-        geo_country
+        geo_country,
+        CASE when is_car_owner = 'true' THEN 1 ELSE 0 END as is_car_owner
     from session_key_added)
    
 
