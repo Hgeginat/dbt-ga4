0e93c1a65acc7ca447e40cd8ed9b0046d4298136
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-03-06 18:41:01 +0000

    comments in derived session properties

--- models/staging/ga4/stg_ga4__derived_session_properties.sql
+++ models/staging/ga4/stg_ga4__derived_session_properties.sql
@@ -1,3 +1,5 @@
+-- looking into dbt_project to see if the variable exist
+
 {{ config(
   enabled = true if var('derived_session_properties', false) else false,
   materialized = "table"
@@ -8,6 +10,8 @@ with events_from_valid_users as (
     select * from {{ref('stg_ga4__events')}}
     where session_key is not null
 ),
+
+-- unnest all user properties & event params to unest based on information in dbt_project file
 unnest_event_params as
 (
     select 
@@ -21,8 +25,13 @@ unnest_event_params as
             {% endif %}
         {% endfor %}
     from events_from_valid_users
+
+
 )
 
+--After unnesting, it selects distinct session keys and applies a window function to calculate the last value of each session property over a session window.
+ -- It iterates over the properties specified in derived_session_properties, applying the window function accordingly.
+ 
 SELECT DISTINCT
     session_key
     {% for sp in var('derived_session_properties', []) %}
