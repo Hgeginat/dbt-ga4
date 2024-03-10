bdcd5003ef322b6c409ccd8a01f53833437efc4b
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-03-10 21:10:51 +0000

    removing the column not needed (computed but for a website)

--- models/staging/ga4/stg_ga4__events.sql
+++ models/staging/ga4/stg_ga4__events.sql
@@ -39,52 +39,7 @@ include_event_key as (
         *,
         to_base64(md5(CONCAT(session_key, event_name, CAST(event_timestamp as STRING), to_json_string(event_params)))) as event_key -- Surrogate key for unique events.  
     from include_session_partition_key
-),
-detect_gclid as (
-    select
-        * except (source, medium, campaign),
-        case
-            when (page_location like '%gclid%' and source is null) then "google"
-            else source
-        end as source,
-        case
-            when (page_location like '%gclid%' and medium is null) then "cpc"
-            else medium
-        end as medium,
-        case
-            when (page_location like '%gclid%' and campaign is null) then "(cpc)"
-            else campaign
-        end as campaign
-    from include_event_key
-),
--- Remove specific query strings from page_location field
-remove_query_params as (
-
-    select 
-        * EXCEPT (page_location, page_referrer),
-        page_location as original_page_location,
-        page_referrer as original_page_referrer,
-        -- If there are query parameters to exclude, exclude them using regex
-        {% if var('query_parameter_exclusions',none) is not none %}
-        {{remove_query_parameters('page_location',var('query_parameter_exclusions'))}} as page_location,
-        {{remove_query_parameters('page_referrer',var('query_parameter_exclusions'))}} as page_referrer
-        {% else %}
-        page_location,
-        page_referrer
-        {% endif %}
-    from detect_gclid
-),
-enrich_params as (
-    select 
-        *,
-        {{extract_hostname_from_url('page_location')}} as page_hostname,
-        {{extract_query_string_from_url('page_location')}} as page_query_string,
-    from remove_query_params
-),
-page_key as (
-    select
-        *,
-        (concat( cast(event_date_dt as string), cast(EXTRACT(HOUR FROM TIMESTAMP_MICROS(event_timestamp)) as string), page_location )) as page_key
-    from enrich_params
 )
-select * from page_key
+-- Remove specific query strings from page_location field
+
+select * from include_event_key
