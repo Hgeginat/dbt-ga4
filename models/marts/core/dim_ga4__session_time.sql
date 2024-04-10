cea4ad3d68c4463617fad8527463a14441d0ef38
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-04-10 10:34:04 +0000

    final final time session

--- models/marts/core/dim_ga4__session_time.sql
+++ models/marts/core/dim_ga4__session_time.sql
@@ -2,7 +2,7 @@ with group1 as (
     select 
         session_key as session_key,
         event_date_dt as date,
-        -- user_key as user_key,
+        user_key as user_key,
         user_pseudo_id,
         device_operating_system,
         -- sum((engagement_time_msec))/1000 as engagement_time_sec,
@@ -11,69 +11,25 @@ with group1 as (
         (max(event_timestamp)- min(event_timestamp))/1000000 as sessiontimesec
 
     from {{ref('stg_ga4__events')}} 
-    group by 1,2,3,4
+    group by 1,2,3,4, 5
    
+),
+-- select * from group1 where sessiontimesec > 0
+
+
+include_user_properties_market as (
+    select
+     group1.*,
+     user_properties.polestar_market,
+     user_properties.logged_in,
+     user_properties.is_paired
+     from group1
+     {% if var('user_properties', false) %}
+    -- If user properties have been assigned as variables, join them on the user_key
+    left join {{ref('stg_ga4__user_properties')}} as user_properties using (user_key)
+    {% endif %}
 )
-select * from group1 where sessiontimesec > 0
- 
--- select * from group1 where sessiontimesec > 0 and user_pseudo_id ='0D8AA365C35D49E5ADCC711B660C2C63'
 
--- include_user_properties_market as (
---     select
---      group1.*,
---      user_properties.polestar_market,
---      user_properties.logged_in,
---      user_properties.is_paired
---      from group1
---      {% if var('user_properties', false) %}
---     -- If user properties have been assigned as variables, join them on the user_key
---     left join {{ref('stg_ga4__user_properties')}} as user_properties using (user_key)
---     {% endif %}
--- ),
--- include_engagement as (
---     select
---     include_user_properties_market.*,
---     session_identifyer.session_engaged as session_eng,
---     session_identifyer.ga_session_id as ga_session_id,
---     session_identifyer.ga_session_number as ga_session_number,
---     from include_user_properties_market
---     left join {{ref('dim_ga4__sessions')}} as session_identifyer using (session_key)
+select * from include_user_properties_market 
 
--- ),
--- unduplicate as (
---     select 
---     date,
---     polestar_market,
---     user_pseudo_id,
---     user_key,
---     session_key,
---     ga_session_id,
---     ga_session_number,
---     event_name,
---     session_eng,
---     engagement_time_sec,
---     logged_in,
---     is_paired,
---     device_operating_system,
---     ROW_NUMBER() over(partition by date,
---                                     CAST(engagement_time_sec as STRING),
---                                     polestar_market,
---                                     user_pseudo_id,
---                                     user_key,
---                                     session_key,
---                                     user_key,
---                                     ga_session_id,
---                                     ga_session_number,
---                                     event_name,
---                                     session_eng,
---                                     logged_in,
---                                     is_paired
-                                    
---             order by date) AS DuplicateCount
---     from include_engagement
--- )
-
--- select * from unduplicate 
-
-
- 
+--  where sessiontimesec > 0
