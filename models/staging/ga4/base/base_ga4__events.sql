e5499d6a80a9704a2ec6aa6c894b5ebab742c6d1
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-07-30 08:58:29 +0000

    just acccepting to delete

--- models/staging/ga4/base/base_ga4__events.sql
+++ models/staging/ga4/base/base_ga4__events.sql
@@ -150,7 +150,7 @@ renamed as (
         platform,
         ecommerce,
         items,
-        -- collected_traffic_source,
+        collected_traffic_source,
         (case when (is_active_user = true) then 1 else 0 end) as active_user_index,
         {{ ga4.unnest_key('event_params', 'ga_session_id', 'int_value') }},
         {{ ga4.unnest_key('event_params', 'ga_session_number',  'int_value') }},
