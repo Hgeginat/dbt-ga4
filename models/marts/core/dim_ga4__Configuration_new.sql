f76c975bb886856bd599183b9833fae644865535
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-09-27 13:18:01 +0000

    configuration update

--- models/marts/core/dim_ga4__Configuration_new.sql
+++ models/marts/core/dim_ga4__Configuration_new.sql
@@ -1,3 +1,41 @@
+{% if var('static_incremental_days', false ) %} -- if true, this part of the code will be running only if the variable static_incremental_days exist. if it does not exist, the default value of it will be false. Therefore, the code in the block will not run
+    {% set partitions_to_replace = [] %} --  Initializes an empty list named partitions_to_replace aim :  This list is intended to store partition values for incremental loading.
+
+    {% for i in range(var('static_incremental_days')) %}   -- loop for 0 to incrementalsday-1
+        {% set partitions_to_replace = partitions_to_replace.append('date_sub(current_date, interval ' + (i+1)|string + ' day)') %} -- getting a list of date where we remove the amount of days from 0 to incrementals days-1
+    {% endfor %}
+
+
+ --  config : This function generates configurations for the data loading process. so the first part here is configuring the upload 
+    {{
+        config(                             
+            materialized = 'incremental',
+            incremental_strategy = 'insert_overwrite',
+            partition_by={
+                "field": "date",
+                "data_type": "date",
+            },
+            partitions = partitions_to_replace,
+        )
+    }}
+{% else %}
+
+--meaning her no 'static_incremental_days' value, and therefore incremental update over here
+    {{
+        config(
+            materialized = 'incremental',
+            incremental_strategy = 'insert_overwrite',
+            partition_by={
+                "field": "date",
+                "data_type": "date",
+            },
+        )
+    }}
+{% endif %}
+
+
+
+
 with events_base2 as (
     select 
         event_name,
@@ -33,13 +71,13 @@ include_derived_session_properties2 as (
 ),
 app_events2 as (
     select 
+        event_date_dt as date,
         event_name,
         content_type,
         car_model,
         item_id,
         item_category,
         {{ga4.car_control_category('content_type')}} as car_control_category,
-        event_date_dt as date,
         polestar_market,
         active_user_index,
         (case when engagement_time_msec > 0 or session_engaged = 1 then user_key else null end) as active_user_key,
