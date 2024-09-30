2f2f05e998e7f29569aa71364087eef3a99f21f2
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-09-30 16:18:17 +0000

    not loggin incremetnal

--- models/marts/core/dim_ga4__NotLoggin_incr.sql
+++ models/marts/core/dim_ga4__NotLoggin_incr.sql
@@ -1,6 +1,41 @@
+{% if var('static_incremental_days', false ) %} -- if true, this part of the code will be running only if the variable static_incremental_days exist. if it does not exist, the default value of it will be false. Therefore, the code in the block will not run
+    {% set partitions_to_replace = [] %} --  Initializes an empty list named partitions_to_replace aim :  This list is intended to store partition values for incremental loading.
+
+    {% for i in range(var('static_incremental_days')) %}   -- loop for 0 to incrementalsday-1
+        {% set partitions_to_replace = partitions_to_replace.append('date_sub(current_date, interval ' + (i+1)|string + ' day)') %} -- getting a list of date where we remove the amount of days from 0 to incrementals days-1
+    {% endfor %}
+
+
+ --  config : This function generates configurations for the data loading process. so the first part here is configuring the upload 
+    {{
+        config(                             
+            materialized = 'incremental',
+            incremental_strategy = 'insert_overwrite',
+            partition_by={
+                "field": "date",
+                "data_type": "date",
+            },
+            partitions = partitions_to_replace,
+        )
+    }}
+{% else %}
+
+--meaning her no 'static_incremental_days' value, and therefore incremental update over here
+    {{
+        config(
+            materialized = 'incremental',
+            incremental_strategy = 'insert_overwrite',
+            partition_by={
+                "field": "date",
+                "data_type": "date",
+            },
+        )
+    }}
+{% endif %}
+
 with screen_name_events2 as (
     select
-        event_date_dt as date,
+        event_date_dt,
         TIMESTAMP_MICROS(event_timestamp) as  time,
         event_name,
         user_key,
@@ -15,7 +50,14 @@ with screen_name_events2 as (
         (select value.string_value from unnest(user_properties) where key = 'logged_in') as logged_in,
         (select value.string_value from unnest(user_properties) where key = 'is_paired') as is_paired
     from {{ref('stg_ga4__events')}}
-    
+    {% if is_incremental() %}
+
+        {% if var('static_incremental_days', false ) %}
+            and event_date_dt in ({{ partitions_to_replace | join(',')}})
+        {% else %}
+            and event_date_dt >= _dbt_max_partition
+        {% endif %}
+    {% endif %}
 ),
 
 include_derived_session_properties2 as (
@@ -30,7 +72,7 @@ include_derived_session_properties2 as (
     {% endif %}
 ),
 Journey as (
-    select date,
+    select event_date_dt as date,
         time,
         event_name,
         user_key,
