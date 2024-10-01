0d508440edac20602c3c5012eca6e2cead1221e2
helena.geginat@bakestrategy.com <helena.geginat@bakestrategy.com>
2024-10-01 09:21:53 +0000

    putting on hold 2 unused fonctions

--- models/marts/core/fct_ga4__sessions.sql
+++ models/marts/core/fct_ga4__sessions.sql
@@ -1,17 +1,17 @@
 -- Stay mindful of performance/cost when leavin this model enabled. Making this model incremental on date is not possible because there's no way to create a single record per session AND partition on date. 
 
-select
-    user_pseudo_id,
-    session_key,
-    min(session_partition_min_timestamp) as session_start_timestamp,
-    min(session_partition_date) as session_start_date,
-    max(session_partition_max_session_engaged) as is_session_engaged,
-    sum(session_partition_sum_engagement_time_msec) as sum_engaged_time_msec
-    {% if var('conversion_events', false) %}
-        {% for ce in var('conversion_events',[]) %}
-            , sum({{ce}}_count) as sum_{{ce}}
-        {% endfor %}
-    {% endif %}
-from {{ref('fct_ga4__sessions_daily')}}
-group by 1,2
+-- select
+--     user_pseudo_id,
+--     session_key,
+--     min(session_partition_min_timestamp) as session_start_timestamp,
+--     min(session_partition_date) as session_start_date,
+--     max(session_partition_max_session_engaged) as is_session_engaged,
+--     sum(session_partition_sum_engagement_time_msec) as sum_engaged_time_msec
+--     {% if var('conversion_events', false) %}
+--         {% for ce in var('conversion_events',[]) %}
+--             , sum({{ce}}_count) as sum_{{ce}}
+--         {% endfor %}
+--     {% endif %}
+-- from {{ref('fct_ga4__sessions_daily')}}
+-- group by 1,2
 
